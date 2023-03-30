082df446f0e34e659e349586bedf8d6f9dfe47fd
Tarcisio Santos Junior <tarcisiodev1@gmail.com>
2023-03-30 00:32:18 -0300

    exercicio 3

---
+++ ex003/cad.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Resultado</title>
+</head>
+
+<body>
+    <header>
+        <h1>Resultado do processamento</h1>
+    </header>
+    <main>
+        <?php
+        // var_dump($_GET); //"$_REQUEST" É A junçao das superglobal: $_GET ,$_POST $_COOKIES
+        $n = $_GET["nome"] ?? "Sem nome";
+        $s = $_GET["sobrenome"] ?? "Desconhecido"; //Operador de coalescência nula
+        echo "<p>É um prazer te conhecer <strong>$n $s</strong>! Este é meu site!</p>"
+        ?>
+        <p><a href="javascript:history.go(-1)">Voltar para a página anterior</a></p>
+    </main>
+</body>
+
+</html>
