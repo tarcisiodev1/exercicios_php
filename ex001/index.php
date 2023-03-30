6229e19bf04d444221f6a4bf05d08f0508e8abad
Tarcisio Santos Junior <tarcisiodev1@gmail.com>
2023-03-29 22:21:22 -0300

    first commit

---
+++ ex001/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<h1>Informações do servidor</h1>
+<?php
+
+echo "hoje é dia " . date("d/M/Y");
+echo " e a hora atual é " . date("G:i:s T");
+phpinfo() ?>
+
+<body>
+
+</body>
+
+</html>
